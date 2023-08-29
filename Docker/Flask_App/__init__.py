c2eb40e30142d879bfcf178336374b26793a9311
M. Said Bilgehan <msaidbilgehan@gmail.com>
2023-08-29 14:29:31 +0300

    Fixed non exist folder issue

--- Docker/Flask_App/__init__.py
+++ Docker/Flask_App/__init__.py
@@ -1,5 +1,14 @@
 import os
-from Flask_App.paths import root_path
+from Flask_App.paths import root_path, root_path_archives, root_log_collection_folder, root_fqdn_folder
 
 if not os.path.exists(root_path):
     os.makedirs(root_path)
+    
+if not os.path.exists(root_path_archives):
+    os.makedirs(root_path_archives)
+    
+if not os.path.exists(root_log_collection_folder):
+    os.makedirs(root_log_collection_folder)
+    
+if not os.path.exists(root_fqdn_folder):
+    os.makedirs(root_fqdn_folder)
